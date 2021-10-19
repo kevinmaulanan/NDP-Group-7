da9d3b20b8661731fe1cbf27e142ac0f3d9e5bcf
Kevin Maulana <kevinmaulanan@gmail.com>
2021-10-19 22:02:58 +0700

    Kevin - Material IO Login & Register Page

--- app/src/main/java/com/example/zesmart/LoginActivity.java
+++ app/src/main/java/com/example/zesmart/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.zesmart;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        TextView textRegister = findViewById(R.id.suruh_register);
+
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindahKeRegister = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(pindahKeRegister);
+            }
+        });
     }
 }
