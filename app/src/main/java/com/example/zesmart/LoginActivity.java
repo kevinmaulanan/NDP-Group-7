afcc970421044d481c3a2d1c86dea6dc676e4d30
Kevin Maulana <kevinmaulanan@gmail.com>
2021-10-28 17:32:11 +0700

    Kevin - On Boarding Page

--- app/src/main/java/com/example/zesmart/LoginActivity.java
+++ app/src/main/java/com/example/zesmart/LoginActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -26,5 +29,25 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(pindahKeRegister);
             }
         });
+
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindahKeDetailMater = new Intent(LoginActivity.this,DetailMateriActivity.class);
+                startActivity(pindahKeDetailMater);
+            }
+        });
+    }
+
+
+    public boolean onKeyDown(int key_code, KeyEvent key_event) {
+        if (key_code== KeyEvent.KEYCODE_BACK) {
+            super.onKeyDown(key_code, key_event);
+            return false;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/zesmart/SplashScreenActivity.java
+++ app/src/main/java/com/example/zesmart/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.zesmart;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
     }
 }
